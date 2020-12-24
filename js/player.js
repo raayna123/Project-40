58d5f7b40cd5d189d7f310b04f8a83f4736ec332
raayna123 <72254862+raayna123@users.noreply.github.com>
2020-12-24 13:48:45 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,52 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    // getScore() {
+    //     var scoreRef = database.ref('score');
+    //     scoreRef.on("value", (data) => {
+    //         score = data.val();
+    //     })
+    // }
+
+    // updateScore(score) {
+    //     database.ref('players/score').update({
+    //         score: score
+    //     });
+    // }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
